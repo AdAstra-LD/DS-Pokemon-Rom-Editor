49a0757fd3f282acac4369a7c2e8069542d771fe
AdAstra-LD <gianni.laiso.gl@gmail.com>
2021-01-25 04:27:56 +0100

    1.0.8

--- DS_Map/Main Window.Designer.cs
+++ DS_Map/Main Window.Designer.cs
@@ -7049,7 +7049,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "MainProgram";
-            this.Text = "DS Pokémon Rom Editor 1.0.7b (Nømura, AdAstra, Mixone)";
+            this.Text = "DS Pokémon Rom Editor 1.0.8 (Nømura, AdAstra, Mixone)";
             this.mainTabControl.ResumeLayout(false);
             this.headerEditorTabPage.ResumeLayout(false);
             this.headerEditorTabPage.PerformLayout();
