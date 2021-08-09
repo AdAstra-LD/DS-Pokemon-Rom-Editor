7422dad225b0aa4747e9869ef6bc479c4e201564
AdAstra-LD <76622070+AdAstra-LD@users.noreply.github.com>
2021-08-09 19:30:04 +0200

    ToolboxDB now takes advantage of the new RomInfo enums
    specifically, gFamEnum and gLangEnum

--- DS_Map/Resources/ROMToolboxDB/ToolboxDB.cs
+++ DS_Map/Resources/ROMToolboxDB/ToolboxDB.cs
@@ -11,51 +11,51 @@ namespace DSPRE.Resources.ROMToolboxDB {
         };
 
         public static Dictionary<string, string> arm9ExpansionCodeDB = new Dictionary<string, string>() {
-            ["branchString_DP_ENG"] = "05 F1 34 FC",
-            ["branchString_DP_ESP"] = "05 F1 04 FD",
-            ["branchString_Plat_ENG"] = "00 F1 B4 F8",
-            ["branchString_Plat_ESP"] = "00 F1 B2 F9",
-            ["branchString_HGSS_ENG"] = "0F F1 30 FB",
-            ["branchString_HGSS_ESP"] = "0F F1 40 FB",
+            ["branchString" + "_" + RomInfo.gFamEnum.DP + "_" + RomInfo.gLangEnum.English] = "05 F1 34 FC",
+            ["branchString" + "_" + RomInfo.gFamEnum.DP + "_" + RomInfo.gLangEnum.Spanish] = "05 F1 04 FD",
+            ["branchString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = "00 F1 B4 F8",
+            ["branchString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = "00 F1 B2 F9",
+            ["branchString" + "_" + RomInfo.gFamEnum.HGSS + "_" + RomInfo.gLangEnum.English] = "0F F1 30 FB",
+            ["branchString" + "_" + RomInfo.gFamEnum.HGSS + "_" + RomInfo.gLangEnum.Spanish] = "0F F1 40 FB",
 
-            ["initString_DP"] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD F1 64 00 02 00 80 3C 02",  //Valid for ENG and ESP, also for P            
-            ["initString_Plat_ENG"] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD A5 6A 00 02 00 80 3C 02",
-            ["initString_Plat_ESP"] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD B9 6A 00 02 00 80 3C 02",
-            ["initString_HGSS"] = "FC B5 05 48 C0 46 1C 21 00 22 02 4D A8 47 00 20 03 21 FC BD 09 75 00 02 00 80 3C 02" //Valid for ENG and ESP, also for SS
+            ["initString" + "_" + RomInfo.gFamEnum.DP] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD F1 64 00 02 00 80 3C 02",  //Valid for ENG and ESP, also for P            
+            ["initString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD A5 6A 00 02 00 80 3C 02",
+            ["initString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = "FC B5 05 48 C0 46 41 21 09 22 02 4D A8 47 00 20 03 21 FC BD B9 6A 00 02 00 80 3C 02",
+            ["initString" + "_" + RomInfo.gFamEnum.HGSS] = "FC B5 05 48 C0 46 1C 21 00 22 02 4D A8 47 00 20 03 21 FC BD 09 75 00 02 00 80 3C 02" //Valid for ENG and ESP, also for SS
         };
         public static Dictionary<string, uint> arm9ExpansionOffsetsDB = new Dictionary<string, uint>() {
-            ["branchOffset_DP"] = 0x02000C80, //Valid also for P
-            ["branchOffset_Plat"] = 0x02000CB4,
-            ["branchOffset_HGSS"] = 0x02000CD0, //Valid also for SS
-
-            ["initOffset_DP_ENG"] = 0x021064EC,
-            ["initOffset_DP_ESP"] = 0x0210668C,
-            ["initOffset_Plat_ENG"] = 0x02100E20,
-            ["initOffset_Plat_ESP"] = 0x0210101C,
-            ["initOffset_HGSS_ENG"] = 0x02110334,
-            ["initOffset_HGSS_ESP"] = 0x02110354
+            ["branchOffset" + "_" + RomInfo.gFamEnum.DP] = 0x02000C80, //Valid also for P
+            ["branchOffset" + "_" + RomInfo.gFamEnum.Plat] = 0x02000CB4,
+            ["branchOffset" + "_" + RomInfo.gFamEnum.HGSS] = 0x02000CD0, //Valid also for SS
+            
+            ["initOffset" + "_" + RomInfo.gFamEnum.DP + "_" + RomInfo.gLangEnum.English] = 0x021064EC,
+            ["initOffset" + "_" + RomInfo.gFamEnum.DP + "_" + RomInfo.gLangEnum.Spanish] = 0x0210668C,
+            ["initOffset" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = 0x02100E20,
+            ["initOffset" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = 0x0210101C,
+            ["initOffset" + "_" + RomInfo.gFamEnum.HGSS + "_" + RomInfo.gLangEnum.English] = 0x02110334,
+            ["initOffset" + "_" + RomInfo.gFamEnum.HGSS + "_" + RomInfo.gLangEnum.Spanish] = 0x02110354
         };
 
         public static Dictionary<string, string> BDHCamCodeDB = new Dictionary<string, string>() {
-            ["branchString_Plat_ENG"] = "B9 F3 E2 F8",
-            ["branchString_Plat_ESP"] = "B9 F3 AA F8",
-            ["branchString_HGSS"] = "B6 F3 2E FA", //Also valid for SS, both ESP and ENG
+            ["branchString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = "B9 F3 E2 F8",
+            ["branchString" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = "B9 F3 AA F8",
+            ["branchString" + "_" + RomInfo.gFamEnum.HGSS] = "B6 F3 2E FA", //Also valid for SS, both ESP and ENG
 
             ["overlayString1"] = "00 4B 18 47 41 9C 3D 02",
             ["overlayString2"] = "00 4B 18 47 01 9C 3D 02",
         };
         public static Dictionary<string, uint> BDHCamOffsetsDB = new Dictionary<string, uint>() {
-            ["branchOffset_Plat_ENG"] = 0x0202040C,
-            ["branchOffset_Plat_ESP"] = 0x0202047C,
-            ["branchOffset_HGSS"] = 0x02023174, //Also valid for SS, both ESP and ENG
+            ["branchOffset" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = 0x0202040C,
+            ["branchOffset" + "_" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = 0x0202047C,
+            ["branchOffset" + "_" + RomInfo.gFamEnum.HGSS] = 0x02023174, //Also valid for SS, both ESP and ENG
 
-            ["overlayOffset1_Plat_ENG"] = 0x0001E1B4,
-            ["overlayOffset1_Plat_ESP"] = 0x0001E1BC,
-            ["overlayOffset1_HGSS"] = 0x0001574C,
+            ["overlayOffset1" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = 0x0001E1B4,
+            ["overlayOffset1" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = 0x0001E1BC,
+            ["overlayOffset1" + RomInfo.gFamEnum.HGSS] = 0x0001574C,
 
-            ["overlayOffset2_Plat_ENG"] = 0x0001E2CC,
-            ["overlayOffset2_Plat_ESP"] = 0x0001E2D4,
-            ["overlayOffset2_HGSS"] = 0x00015864,
+            ["overlayOffset2" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.English] = 0x0001E2CC,
+            ["overlayOffset2" + RomInfo.gFamEnum.Plat + "_" + RomInfo.gLangEnum.Spanish] = 0x0001E2D4,
+            ["overlayOffset2" + RomInfo.gFamEnum.HGSS] = 0x00015864,
         };
         public static uint BDHCamSubroutineOffset = 0x000115B0;
 
