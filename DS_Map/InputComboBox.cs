2dbb5af38799d398454f6537edb92dbb03eb4e21
AdAstra-LD <76622070+AdAstra-LD@users.noreply.github.com>
2023-04-18 16:36:37 +0200

    Created Custom InputComboBox

---
+++ DS_Map/InputComboBox.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace DSPRE {
+    public partial class InputComboBox : ComboBox {
+        public InputComboBox() {
+            DropDownStyle = ComboBoxStyle.DropDown;
+
+            AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            AutoCompleteSource = AutoCompleteSource.ListItems;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e) {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.Enter) {
+                string input = Text;
+                int index = FindStringExact(input.Trim());
+                if (index != -1) {
+                    SelectedIndex = index;
+                }
+            }
+        }
+
+        [Browsable(false)]
+        public new ComboBoxStyle DropDownStyle {
+            get { return base.DropDownStyle; }
+            set { base.DropDownStyle = ComboBoxStyle.DropDown; }
+        }
+    }
+
+}
