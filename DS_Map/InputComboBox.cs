cbfa1b00b62c20b809ef9b02752a0a3fe510e78c
AdAstra-LD <76622070+AdAstra-LD@users.noreply.github.com>
2023-05-13 17:00:18 +0200

    Update error check of InputComboBox

--- DS_Map/InputComboBox.cs
+++ DS_Map/InputComboBox.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -14,19 +15,26 @@ namespace DSPRE {
             AutoCompleteSource = AutoCompleteSource.ListItems;
         }
 
+        private void UpdateText() {
+            string input = Text;
+            int index = FindStringExact(input.Trim());
+            if (index == -1) {
+                this.BackColor = Color.IndianRed;
+            } else {
+                this.BackColor = normalColor;
+                SelectedIndex = index;
+            }
+        }
         protected override void OnKeyDown(KeyEventArgs e) {
             base.OnKeyDown(e);
 
             if (e.KeyCode == Keys.Enter) {
-                string input = Text;
-                int index = FindStringExact(input.Trim());
-                if (index == -1) {
-                    this.BackColor = Color.IndianRed;
-                } else {
-                    this.BackColor = normalColor;
-                    SelectedIndex = index;
-                }
+                UpdateText();
             }
+        }
+        protected override void OnLeave(EventArgs e) {
+            base.OnLeave(e);
+            UpdateText();
         }
 
         [Browsable(false)]
