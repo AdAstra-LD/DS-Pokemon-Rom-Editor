f2688a9b8d97a1775371439e6ecc3383415b26aa
AdAstra-LD <76622070+AdAstra-LD@users.noreply.github.com>
2023-09-18 14:43:20 +0200

    Version update

--- DS_Map/Main Window.Designer.cs
+++ DS_Map/Main Window.Designer.cs
@@ -11600,7 +11600,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "MainProgram";
-            this.Text = "DS Pokémon Rom Editor Reloaded 1.9.2 (Nømura, AdAstra/LD3005)";
+            this.Text = "DS Pokémon Rom Editor Reloaded 1.10 (Nømura, AdAstra/LD3005, Mixone)";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainProgram_FormClosing);
             this.mainTabControl.ResumeLayout(false);
             this.headerEditorTabPage.ResumeLayout(false);
