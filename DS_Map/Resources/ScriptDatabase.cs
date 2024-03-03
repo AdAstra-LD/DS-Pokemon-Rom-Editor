9b28d63e1e9244705071944de21d31f1491a9b9f
Miguel Terol Espino <miguel.terol.93@gmail.com>
2024-03-03 21:42:26 +0100

    added alpha ruin command names. needs checking asm

--- DS_Map/Resources/ScriptDatabase.cs
+++ DS_Map/Resources/ScriptDatabase.cs
@@ -2094,6 +2094,9 @@ namespace DSPRE.Resources {
 
             [0x02C7] = "TalkFollowingPoke",
 
+            [0x02C9] = "OpenAlphaPuzzle",
+            [0x02CA] = "OpenRuinsofAlphHiddenRoom",
+
             [0x02D3] = "BoatAnimation",
 
 
