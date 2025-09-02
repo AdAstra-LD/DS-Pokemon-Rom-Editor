ca5f7c5c8f4bfb045b007ba85dbba48de4a9ab90
Yako <yakoswg@gmail.com>
2025-09-03 00:41:30 +0200

    Hide the add / remove buttons in trade editor until implemented (#98)
    resolves https://github.com/Mixone-FinallyHere/DS-Pokemon-Rom-Editor/issues/94

--- DS_Map/Editors/TradeEditor.cs
+++ DS_Map/Editors/TradeEditor.cs
@@ -66,6 +66,8 @@ namespace DSPRE.Editors
             // Disable buttons until trade expansion is implemented
             addFileButton.Enabled = false;
             removeLastButton.Enabled = false;
+            addFileButton.Visible = false;
+            removeLastButton.Visible = false;
 
             tradeArchive = new TextArchive(GetTextBankIndex());
             LoadFromFile(0);
